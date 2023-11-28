07f44db07278db913b9a2e28228847dbaeccb576
Khaloud <hamedkhaloud71@gmail.com>
2023-11-28 11:05:34 +0400

    adding Some Changes inside Class

--- Week3-OOP/Employee.cs
+++ Week3-OOP/Employee.cs
@@ -12,19 +12,26 @@ namespace Week3_OOP
         public int age;
         public decimal Salary;
 
-        //constacter ==> is have same name of my class 
+
+        //constacter ==> is have same name of my class it can be empty || or have variable
         public Employee()
         {
+            //empty constacter
+        }
+        // ctor + tab ==> اختصار 
+
+
+        public Employee(string _FName, string _LName, int age )
+        {
+            // constacter with vairable
+            FName = _FName;
+            LName = _LName;
+          
+            this.age = age;
+            // we use this before it to now wich ne is from class and other one from constacter
+
 
         }
-
-        public Employee(string _FName, string _LName, int _age)
-        {
-          FName = _FName;
-          LName = _LName;
-          age = _age;
-
-        } 
 
     }
 
